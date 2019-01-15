180d609b17440af1e764c65ed7dc62ca0878e2a8
Shoshana Rosenfield <Shoshana@dhcp-nyc-119.wifi-visitor.cit.cornell.edu>
2019-01-15 17:45:37 -0500

    update to include submit button and react form to make axios get request to giphy api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      data: [],
+      searchInput: ''
+    }
+   this.handleChange = this.handleChange.bind(this)
+   this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+    console.log('this.state',this.state)
+  }
+  async handleSubmit(event){
+    event.preventDefault()
+    const query=this.state.searchInput
+    const {data} = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=HkfXFPHQMKATUUpd2DrfSqy5QNY98nUS&limit=5`)
+    console.log('data',data)
+    this.setState({
+      data,
+      searchInput: ''
+    })
+
+  }
+
   render() {
     return (
       <div id='header'>
@@ -9,8 +38,12 @@ class App extends Component {
             Giphy Fun
           </h1>
       <div>
-        <input id='searchBar' type="text" placeholder="Search.." />
+        <form onSubmit={this.handleSubmit}>
+        <input name='searchInput'id='searchBar' type="text" placeholder="Search.." onChange={this.handleChange}/>
+        <button type='submit'>Submit</button>
+        </form>
       </div>
+
       </div>
     );
   }
